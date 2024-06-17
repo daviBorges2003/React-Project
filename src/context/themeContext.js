2641a6335656be2bb6c8a1b4cdf875a6d1814311
daviBorges2003 <davi.paula@hotmail.com>
2024-06-17 17:31:49 -0300

    Alteração de Contexto

--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -1,31 +1,28 @@
 import React, { useState } from 'react';
-import { Container } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Box, Container } from '@mui/material';
+import { ThemeProvider, createTheme, styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Header from '../components/Header';
+import Header from '../components/organisms/Header';
+import BgimgDark from '../img/BgimgDark.png';
+import BgimgLight from '../img/BgimgLight.png';
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
     primary: {
-      main: '#BFC2D9',
-      light: '#7F8AD9',
-      dark: '#7F8AD9',
-      contrastText: '#000',
+      main: '#2055E6',
+      light: '#2093E6',
+      dark: '#112C77',
+      card: '#302F2F',
+      contrastText: '#fff',
     },
   },
-  components: {
-    MuiCard: {
-      styleOverrides: {
-        root: {
-          padding: '10px',
-          display: 'flex',
-          alignItems: 'center',
-          boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.2)',
-          borderRadius: 15,
-        },
-      },
-    },
+  text: {
+    primary: '#FFF',
+  },
+  background: {
+    default: '#222222',
+    image: BgimgDark,
   },
   shadows: {
     1: '0px 10px 15px -3px rgba(0,0,0,0.2)',
@@ -36,35 +33,32 @@ const light = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: '#BFC2D9',
-      light: '#7F8AD9',
-      dark: '#7F8AD9',
-      contrastText: '#000',
-    },
-    background: {
-      default: '#ebebeb',
-    },
-    text: {
-      primary: '#737171',
-    },
-    shadows: {
-      1: '0px 10px 15px -3px rgba(0,0,0,0.2)',
+      main: '#2055E6',
+      light: '#2093E6',
+      dark: '#112C77',
+      contrastText: '#fff',
+
     },
   },
-  components: {
-    MuiCard: {
-      styleOverrides: {
-        root: {
-          padding: '10px',
-          display: 'flex',
-          alignItems: 'center',
-          boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.2)',
-          borderRadius: 15,
-        },
-      },
-    },
+  background: {
+    default: '#ebebeb',
+    image: BgimgLight,
+  },
+  text: {
+    primary: '#222222',
+  },
+  shadows: {
+    1: '0px 10px 15px -3px rgba(0,0,0,0.1)',
   },
 });
+
+const CustomBox = styled(Box)(({ theme }) => ({
+  backgroundImage: `url(${theme.palette.mode === 'dark' ? BgimgDark : BgimgLight})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  width: '100vw',
+  height: '100vh',
+}));
 
 export default function ThemeContext({ children }) {
   const [temas, setTemas] = useState(true);
@@ -75,9 +69,11 @@ export default function ThemeContext({ children }) {
     <ThemeProvider
       theme={temas === true ? light : darkTheme}
     >
-      <CssBaseline />
-      <Header Handle={HandleTheme} />
-      <Container maxWidth="md">{children}</Container>
+      <CustomBox>
+        <CssBaseline />
+        <Header Handle={HandleTheme} />
+        <Container maxWidth="md">{children}</Container>
+      </CustomBox>
     </ThemeProvider>
   );
 }
