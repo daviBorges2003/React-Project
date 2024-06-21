57ab5382cfc595513b5cb42f7f4fc590b13370fb
daviBorges2003 <davi.paula@hotmail.com>
2024-06-21 15:41:09 -0300

    Criação no BoxStatus

---
+++ src/components/moleculas/StatusBox/StatusBox.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import { Box } from '@mui/material';
+
+import TitleTexts from '../../atoms/Texts/TitleTexts';
+import BodyText from '../../atoms/Texts/BodyText';
+
+function StatusBar({
+  w, status, title, children,
+}) {
+  const CustomStatus = styled(Box)(({ theme }) => ({
+    width: w || '100%',
+    height: '100%',
+    padding: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: status === 'error' ? theme.palette.error.light : theme.palette.success.light,
+    color: theme.palette.primary.contrastText,
+    border: `5px solid ${status === 'error' ? theme.palette.error.main : theme.palette.success.main}`,
+  }));
+
+  return (
+    <Box>
+      <CustomStatus
+        width={w}
+      >
+        <TitleTexts align="start" font="25px">
+          {title}
+        </TitleTexts>
+        <BodyText>
+          {children}
+        </BodyText>
+      </CustomStatus>
+    </Box>
+  );
+}
+
+export default StatusBar;
